837c845fec7a98ff11523f1f40de69c43659f839
Gutemberg Lima <j.berglima05@gmail.com>
2023-07-02 10:50:54 -0300

    Created Db and Columns

---
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,21 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+    }
+}
--- EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
         public string LivroEmprestado { get; set; }
         public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
 
