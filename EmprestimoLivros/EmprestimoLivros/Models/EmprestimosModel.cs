61d4456c5c91773a21793ba3dd97f0abf210a42a
Gutemberg Lima <j.berglima05@gmail.com>
2023-07-02 09:39:13 -0300

    Project created and code-first DbContext

---
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ EmprestimoLivros/EmprestimoLivros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmprestimoLivros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
